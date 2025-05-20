4e1b6a4eac41c100527fdbeb35ff591e4bd98005
Dmtro_Tr <Di4gamegem@gmail.com>
2025-05-20 14:44:32 +0300

    DachboardButton on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import DachboardButton from '@/app/components/dachboard-button';
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-xl">Home page</h1>
+    <main className="flex flex-col justify-center items-center gap-4">
+      <h1 className="text-xl ">Home page</h1>
+      <DachboardButton />
     </main>
   );
 }
